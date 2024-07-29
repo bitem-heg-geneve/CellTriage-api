46a400682bc0b9dc4a89bbe576df2cec73cdf559
Alexandre Flament <alexandre.flament@hesge.ch>
2024-07-29 16:01:55 +0200

    Remove python-jose unmaintained & unused dependency
    The webapp does not use JWT
    
    python-jose seems unmaintained:
    * https://github.com/mpdavis/python-jose/issues/340
    * CVE --> https://github.com/mpdavis/python-jose/issues/346
    
    Possible remplacement:
    * pyjwt: https://github.com/jpadilla/pyjwt
    * joserfc: https://github.com/authlib/joserfc

--- backend/app/app/api/deps.py
+++ backend/app/app/api/deps.py
@@ -1,17 +1,12 @@
 from typing import Generator, Optional
 
 from fastapi import Depends, HTTPException, status
-from jose import jwt, JWTError
 from pydantic import BaseModel
 from sqlalchemy.orm.session import Session
 
 from app.core.config import settings
 from app.db.session import SessionLocal
 from app import crud
-
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
 
 
 def get_db() -> Generator:
